24de137a31abfc5206ebaed8e5045966332ee370
Reza-84 <reza.masoumifard84@gmail.com>
2025-05-12 22:44:35 +0430

    wish list page added

---
+++ src/components/Wishlist/WishListHeader.jsx
@@ -0,0 +1,20 @@
+export default function WishListHeader(){
+    return<>
+        <header className="header shadow header-fixed border-0">
+			<div className="container">
+				<div className="header-content">
+					<div className="left-content">
+						<a href="javascript:void(0);" className="back-btn">
+							<i className="icon feather icon-chevron-left"></i>
+						</a>
+						<h6 className="title">Wishlist</h6>
+					</div>
+					<div className="mid-content">
+					</div>
+					<div className="right-content">
+					</div>
+				</div>
+			</div>
+		</header>
+    </>
+}
---
+++ src/components/Wishlist/WishListPage.jsx
@@ -0,0 +1,9 @@
+import WishListHeader from "./WishListHeader";
+import WishListMain from "./WishListMain";
+
+export default function WishListPage(){
+    return<>
+        <WishListHeader />
+        <WishListMain />
+    </>
+}
