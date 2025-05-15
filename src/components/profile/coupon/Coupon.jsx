dc8f1c602945ad8dc9220f1dc9218b95e7ad6463
sajjad-em] <amirmohamadsafa2@gmail.com>
2025-05-15 16:15:42 +0330

    add className

--- src/components/profile/coupon/Coupon.jsx
+++ src/components/profile/coupon/Coupon.jsx
@@ -2,76 +2,76 @@ import { Link } from "react-router-dom";
 
 export default function Coupon() {
     return <>
-    <div class="page-wrapper">
+    <div className="page-wrapper">
 
-		<header class="header shadow header-fixed border-0">
-			<div class="container">
-				<div class="header-content">
-					<div class="left-content">
+		<header className="header shadow header-fixed border-0">
+			<div className="container">
+				<div className="header-content">
+					<div className="left-content">
 						<a href="javascript:void(0);">
-							<Link to="/profile" class="back-btn">
-							<i class="icon feather icon-chevron-left"></i>
+							<Link to="/profile" className="back-btn">
+							<i className="icon feather icon-chevron-left"></i>
 							</Link>
 						</a>
-						<h6 class="title">My Coupons</h6>
+						<h6 className="title">My Coupons</h6>
 					</div>
-					<div class="mid-content">
+					<div className="mid-content">
 					</div>
-					<div class="right-content">
+					<div className="right-content">
 					</div>
 				</div>
 			</div>
 		</header>
 
-	<div class="page-content space-top">
-		<div class="container">
-			<div class="dz-offer-coupon">
-				<div class="offer">
+	<div className="page-content space-top">
+		<div className="container">
+			<div className="dz-offer-coupon">
+				<div className="offer">
 					<h6>20%</h6>
 					<span>Off</span>
 				</div>
-				<div class="offer-content">
+				<div className="offer-content">
 					<span>Home Decor</span>
 					<p>On minimum purchase of Rs. 1,999</p>
 				</div>	
 			</div>
-			<div class="dz-offer-coupon">
-				<div class="offer">
+			<div className="dz-offer-coupon">
+				<div className="offer">
 					<h6>50%</h6>
 					<span>Off</span>
 				</div>
-				<div class="offer-content">
+				<div className="offer-content">
 					<span>Home Furnishing</span>
 					<p>On minimum purchase of Rs. 3,999</p>
 				</div>	
 			</div>
-			<div class="dz-offer-coupon">
-				<div class="offer">
+			<div className="dz-offer-coupon">
+				<div className="offer">
 					<h6>25%</h6>
 					<span>Off</span>
 				</div>
-				<div class="offer-content">
+				<div className="offer-content">
 					<span>Mobile Accessories</span>
 					<p>On minimum purchase of Rs. 999</p>
 				</div>	
 			</div>
-			<div class="title-bar mt-4">
-				<h6 class="title mb-0">Discount on your sale</h6>
+			<div className="title-bar mt-4">
+				<h6 className="title mb-0">Discount on your sale</h6>
 			</div>
-			<div class="swiper offer-swiper mb-3">
-				<div class="swiper-wrapper">
-					<div class="swiper-slide">
-						<div class="dz-media">
+			<div className="swiper offer-swiper mb-3">
+				<div className="swiper-wrapper">
+					<div className="swiper-slide">
+						<div className="dz-media">
 							<img src="/images/offer/offer3.png" />
 						</div>
 					</div>
-					<div class="swiper-slide">
-						<div class="dz-media">
+					<div className="swiper-slide">
+						<div className="dz-media">
 							<img src="/images/offer/offer2.png" />
 						</div>
 					</div>
-					<div class="swiper-slide">
-						<div class="dz-media">
+					<div className="swiper-slide">
+						<div className="dz-media">
 							<img src="/images/offer/offer1.png" />
 						</div>
 					</div>	
