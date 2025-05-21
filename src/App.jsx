cbb0e5a7e89239d8ba21a8304675d717523b8a9d
ALYAS AKBARI <elyas1388.akbari@gmail.com>
2025-05-21 17:08:25 +0330

    push all changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,13 @@
-import { useState } from "react";
-import Header from "./components/home/HomeHeader";
 import MenuBar from "./components/MenuBar";
-import SideBar from "./components/home/SideBar";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-// import Routes from "./utils/Routes";
-import Main from "./components/home/Main";
-import HomePage from "./components/home/HomePage";
-import CategoriMain from "./components/Categories/CategoriMain";
-import CategoriPage from "./components/Categories/CategoriPage";
-import Profile from "./components/profile/Profile";
-import WishListPage from "./components/Wishlist/WishListPage";
-import EditProfile from "./components/profile/EditProfile";
-import CartPage from "./components/cart/CartPage";
-import OrderPage from "./components/profile/order/OrderPage";
-import Coupon from "./components/profile/coupon/Coupon";
-import Login from "./public/Login";
-import Welcome from "./public/Welcome";
-import Register from "./public/Register";
-import ForgetPassword from "./public/ForgetPassword";
-import FAQ from "./components/profile/HelpCenter/Help";
-import Address from "./components/profile/address/Address";
-import CheckOut from "./components/profile/address/checkout";
-
+import { BrowserRouter } from "react-router-dom";
+import Routes from "./utils/Routes";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <MenuBar />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/categori" element={<CategoriPage />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/wishlist" element={<WishListPage />} />
-          <Route path="/EditProfile" element={<EditProfile />} />
-          <Route path="/CartPage" element={<CartPage />} />
-          <Route path="/WishListPage" element={<WishListPage />} />
-          <Route path="/OrderPage" element={<OrderPage />} />
-          <Route path="/coupon" element={<Coupon />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/welcome" element={<Welcome />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/forgetPassword" element={<ForgetPassword />} />
-          <Route path="/help" element={<FAQ/>} />
-          <Route path="/address" element={<Address/>} />
-          <Route path="/checkout" element={<CheckOut/>} />
-          
-
-        </Routes>
+        <Routes />
       </BrowserRouter>
     </>
   );
