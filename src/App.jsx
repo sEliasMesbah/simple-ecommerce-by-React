974b7c4ae5e716a85ccf61e8e0dec667a300c61f
Arshid <saediarshid-1385@gmail.com>
2025-05-22 04:04:25 -0700

    added to

--- src/App.jsx
+++ src/App.jsx
@@ -55,41 +55,7 @@ function App() {
       <BrowserRouter>
         <MenuBar />
         <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/categori" element={<CategoriPage />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/wishlist" element={<WishListPage />} />
-          <Route path="/EditProfile" element={<EditProfile />} />
-          <Route path="/CartPage" element={<CartPage />} />
-          <Route path="/WishListPage" element={<WishListPage />} />
-          <Route path="/ComponentPage" element={<ComponentPage/>}/>
-        <Route path="/ui-accordion" element={<Accordion/>}/>
-        <Route path="/ui-alert" element={<Alert/>}/>
-        <Route path="/ui-avatar" element={<Avatar/>}/>
-        <Route path="/ui-badge" element={<Badge/>}/>
-        <Route path="/ui-breadcrumb" element={<Breadcrumb/>}/>
-        <Route path="/ui-button" element={<Button/>}/>
-        <Route path="/ui-button-group" element={<ButtonGroup/>}/>
-        <Route path="/ui-card" element={<Card1/>}/>
-        <Route path="/ui-inputs" element={<Inputs1/>}/>
-        <Route path="/ui-dropdown" element={<Dropdown/>}/>
-        <Route path="/ui-collapse" element={<Collapse/>}/>
-        <Route path="/ui-radio" element={<Radio/>}/>
-        <Route path="/ui-timeline" element={<Timeline/>}/>
-        <Route path="/ui-list-group" element={<ListGroup/>}/>
-        <Route path="/ui-tab" element={<Tab/>}/>
-        <Route path="/ui-divider" element={<Divider/>}/>
-        <Route path="/ui-stepper" element={<Stepper/>}/>
-        <Route path="/ui-offcanva" element={<Offcanvas/>}/>
-        <Route path="/ui-pagination" element={<Pagination/>}/>
-        <Route path="/ui-placeholder" element={<Placeholder/>}/>
-        <Route path="/ui-progress" element={<Progress/>}/>
-        <Route path="/ui-popover" element={<Popover/>}/>
-        <Route path="/ui-scrollspy" element={<Scrollspy/>}/>
-        <Route path="/ui-spinners" element={<Spinners/>}/>
-        <Route path="/ui-toast" element={<Toast/>}/>
-        <Route path="/ui-typography" element={<Typography/>}/>
-        <Route path="/ui-lightgallery" element={<Lightgallery/>}/>
+         
      
 
 
