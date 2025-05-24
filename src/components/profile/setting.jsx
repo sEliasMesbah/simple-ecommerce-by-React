ac9032dc08c3197de5a62ff142659256e0631908
sajjad-em] <amirmohamadsafa2@gmail.com>
2025-05-24 13:50:31 +0330

    EDIT PROFILE

--- src/components/profile/setting.jsx
+++ src/components/profile/setting.jsx
@@ -4,9 +4,18 @@ export default function Setting() {
   return (
     <div className="setting-container">
       <ul>
-        <li>Setting</li>
+        <li>Setting
+          <Link>
+            <i style={{ color: 'var(--primary)', padding: '0 10px' }} class="fa fa-cog" aria-hidden="true"></i>
+            
+          </Link>
+        </li>
+
         <li className="LogOut-btn">
-          <Link to="/login">Log Out</Link>
+          <Link to="/login">Log Out
+            <i style={{ color: 'var(--primary)', padding: '0 10px' }} class="fa fa-sign-out" aria-hidden="true"></i>
+            
+          </Link>
         </li>
       </ul>
     </div>
