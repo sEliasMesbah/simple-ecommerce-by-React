b77b3cd50bd6e35c1c324fc019674b2a91d3f2a1
sajjad-em] <amirmohamadsafa2@gmail.com>
2025-05-27 10:36:26 +0330

    edite option panel

--- src/components/profile/Setting.jsx
+++ src/components/profile/Setting.jsx
@@ -15,38 +15,9 @@ export default function Setting() {
 
   return (
     <div
-      className="setting-dropdown"
-      style={{
-        position: "absolute",
-        top: "calc(100% + 8px)",
-        right: 0,
-        width: "220px",
-        backgroundColor: "white",
-        boxShadow: "0 2px 8px rgba(0,0,0,0.15)",
-        borderRadius: "6px",
-        padding: "10px 0",
-        zIndex: 1000,
-
-
-      }}
-    >
-      <ul
-        style={{
-          listStyle: "none",
-          margin: 0,
-          padding: 0,
-        }}
-      >
-        <li
-          style={{
-            padding: "10px 20px",
-            borderBottom: "1px solid #eee",
-            fontWeight: "bold",
-            color: "#333",
-            transition: '.5s',
-            fontSize: '1.2rem'
-          }}
-        >
+      className="setting-dropdown">
+      <ul>
+        <li>
           Contact
           <Link to="#" style={{ float: "right", color: "var(--primary)" }}>
             <i class="fa fa-address-book" aria-hidden="true"
